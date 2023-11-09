f2a3c27cfc1075597f960ec2c30a3826cae69462
Jose Dominguez <141463676+jdomi020@users.noreply.github.com>
2023-11-09 22:05:10 +0000

    need to link font awesome to react

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
+ 
+	//Add into array -> concat
+	//Delete from array -> filter
+	//Update -> map
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>My Todos</h1>
+			<ul>
+				<li>
+					<input 
+						type="text"
+						onChange={(e) => setInputValue(e.target.value)}
+						value={inputValue}
+						onKeyPress={(e) => {
+							if(e.key === "Enter") {
+						 		setTodos(todos.concat([inputValue]));
+								setInputValue("");
+							}
+						}}
+						placeholder="what do you need to do?"></input><i class="fa-solid fa-trash-can"></i>
+				</li>
+
+				{todos.map((item, index) => (
+					<li>
+						{item}{""} 
+						<i 
+							class="fa-solid fa-trash-can"
+							onClick ={() =>
+								setTodos(
+									todos.filter(
+										(t, currentIndex) =>
+											 index != currentIndex
+									)
+								)
+							}></i>
+					</li>
+				))}
+				
+				
+			</ul>
+			<div>{todos.length} tasks</div>
 		</div>
 	);
 };
