cdfe1ccc7f34f74d094c2786d1d81a3fcdf5b3fe
Jose Dominguez <141463676+jdomi020@users.noreply.github.com>
2023-11-24 21:33:17 +0000

    added my fetch commands, now trying to figure out why the URL isnt working?

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,83 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 
 //create your first component
 const Home = () => {
 	const [inputValue, setInputValue] = useState("");
 	const [todos, setTodos] = useState([]);
+
+	// This is where I'm allegedly creating my username in the API
+
+	useEffect (() => 
+	fetch("https://playground.4geeks/apis/fake/todos/user/", {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json"
+		}
+	})
+	.then(resp => {
+		console.log(resp.ok);
+		console.log(resp.status);
+		console.log(resp.text());
+		return resp.json();
+	})
+	.then(data => {
+		setTodos(data);
+	})
+	.catch(error => {
+		console.log(error)
+	})
+	, []);
+	
+
+	// This would be where I'm putting whatever comes up in the to-list, probably in the body
+
+	useEffect (() =>
+	fetch("https://playground.4geeks/apis/fake/todos/user/jdomi020", {
+		method: "POST",
+		body: JSON.stringify({label: inputValue}),
+		headers: {
+			"Content-Type": "application/json"
+		}
+	})
+	.then(resp => resp.json ()
+		// console.log(resp.ok),
+		// console.log(resp.status),
+		// console.log(resp.text()),
+		// return resp.json()
+	)
+	.then(data => {
+		console.log(data);
+	})
+	.catch(error => {
+		console.log(error)
+	}) 
+	, [inputValue]);
+	
+
+	// and this would allegedly be where i would be deleting the things that I input
+	
+	// useEffect (() => 
+	// fetch("https://playground.4geeks/apis/fake/todos/user/jdomi020", {
+	// 	method: "DELETE",
+	// 	body: [],
+	// 	headers: {
+	// 		"Content-Type": "application/json"
+	// 	}
+	// })
+	// .then(resp => {
+	// 	console.log(resp.ok);
+	// 	console.log(resp.status);
+	// 	console.log(resp.text());
+	// 	return resp.json();
+	// })
+	// .then(data => {
+	// 	console.log(data);
+	// })
+	// .catch(error => {
+	// 	console.log(error)
+	// })
+	// ,[]);
  
 	//Add into array -> concat
 	//Delete from array -> filter
@@ -19,7 +92,7 @@ const Home = () => {
 						type="text"
 						onChange={(e) => setInputValue(e.target.value)}
 						value={inputValue}
-						onKeyPress={(e) => {
+						onKeyDown={(e) => {
 							if(e.key === "Enter") {
 						 		setTodos(todos.concat([inputValue]));
 								setInputValue("");
@@ -29,7 +102,7 @@ const Home = () => {
 				</li>
 
 				{todos.map((item, index) => (
-					<li>
+					<li className="todos">
 						{item}{""} 
 						<i 
 							class="fa-solid fa-trash-can"
